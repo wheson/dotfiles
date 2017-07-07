a9d83cd982d84a702eee6f4157181df236b81b5e
wheson <wheson@whesonMac.local>
2017-07-07 16:17:10 +0900

    add config.fish

--- .zshrc
+++
@@ -1,2 +0,0 @@
-export TERM='xterm-256color'
-export XDG_CONFIG_HOME=~/.config
---
+++ dotfilesLink.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/colors ~/.config/nvim/colors
+ln -sf ~/dotfiles/snippets ~/.config/nvim/snippets
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
