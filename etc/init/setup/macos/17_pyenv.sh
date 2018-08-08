88a5dba7c726a83d78e713089f09728411f46675
wheson <wheson.hxs@gmail.com>
2018-08-08 15:01:02 +0900

    修正

--- etc/init/setup/macos/17_pyenv.sh
+++ etc/init/setup/macos/17_pyenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if type "anyenv" > /dev/null 2>&1 then
+if type "anyenv" > /dev/null 2>&1; then
   anyenv install pyenv
-  if type "pyenv" > /dev/null 2>&1 then
+  if type "pyenv" > /dev/null 2>&1; then
     # default 3.6.5
     pyenv install 3.6.5
     pyenv global 3.6.5
